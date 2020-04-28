0dc2f1e2c039a8f92bc9a61c27c9c9b723641977
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-04-27 23:43:23 -0400

    Time Block divs complete, working on getting the jScript and local storage pieces working.

---
+++ script.js
@@ -0,0 +1,16 @@
+var NowMoment = moment().format("dddd, MMMM Do");
+
+var eDisplayMoment = document.getElementById('currentDay');
+eDisplayMoment.innerHTML = NowMoment;
+
+
+//function to check the timeslot relative to moment.
+function (event){
+
+    //it checks the time within the hour element, to see where it falls within moment, and color .description accordingly.
+}
+
+//function for local storage of 
+function (event){
+
+}
