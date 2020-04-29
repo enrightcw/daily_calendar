b15a077dac6d554b27644ab400d69ef2ae367739
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-04-28 23:04:26 -0400

    Latest updates, added most HTML elements into JScript, layout needs work and local storage isn't set up completely.

--- script.js
+++ script.js
@@ -1,16 +1,72 @@
+//moment
 var NowMoment = moment().format("dddd, MMMM Do");
-
 var eDisplayMoment = document.getElementById('currentDay');
 eDisplayMoment.innerHTML = NowMoment;
 
 
-//function to check the timeslot relative to moment.
-function (event){
+$(document).ready(function() {
+
+    //var declarations
+    
+    var schedule = { time : ["9A", "10A", "11A", "12P", "1P","2P","3P", "4P", "5P"], 
+    userEntry : ["","","","","","","","","",], 
+    hour : ["(moment().hour(9);)" , ]
+    };
+
+
+    //function to check the timeslot relative to moment.
+    // function (event){
 
     //it checks the time within the hour element, to see where it falls within moment, and color .description accordingly.
-}
+    // }
+    function render(){
+        for( let i = 0 ; i < schedule.time.length ; i++) {
+            var divEl = $("<div>");
+            divEl.addClass("row hour");
+            var h4El = $("<h4>");
+            h4El.addClass("timeline");
+            var inputEl = $("<input>");
+            inputEl.addClass("description");
+            var buttonEl = $("<button>").attr("data-input", schedule.time.length[i]).addClass("btn btn-default far fa-save saveBtn");
+            $(h4El).text(schedule.time[i]);
+            
+            
+            
+            $(".time-block").append(divEl);
+            $(divEl).append(h4El);
+            $(divEl).append(inputEl);
+            $(divEl).append(buttonEl);
 
-//function for local storage of 
-function (event){
+            
 
-}
+        }
+
+
+    };
+
+    //function for local storage of descInput, saves to local storage as a string
+    $(".saveBtn").on("click", function(){
+        console.log("__________________")
+        console.log("clicked");
+        console.log(schedule.hour[0])
+        
+        
+        var descInput = document.querySelector(".description").value;
+        console.log(descInput);
+        localStorage.setItem("description", descInput);
+        console.log(descInput);
+    });
+
+    
+    //onclick event for save button
+
+    //Before verification
+    //moment().isBefore(Moment|String|Number|Date|Array);
+
+    //Present verification
+    //moment().isSame(Moment|String|Number|Date|Array);
+
+    //After verification
+    //moment().isAfter(Moment|String|Number|Date|Array);
+    render()
+});
