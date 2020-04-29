55767c9a8d19c01b647141485a6753e4c2c81c09
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-04-29 11:40:09 -0400

    We've got colors based on moment!  Need to get the .present one working though

--- script.js
+++ script.js
@@ -7,10 +7,31 @@ eDisplayMoment.innerHTML = NowMoment;
 $(document).ready(function() {
 
     //var declarations
+    var now = moment();
     
-    var schedule = { time : ["9A", "10A", "11A", "12P", "1P","2P","3P", "4P", "5P"], 
-    userEntry : ["","","","","","","","","",], 
-    hour : ["(moment().hour(9);)" , ]
+    var schedule = { 
+        time : [
+            "9:00am", 
+            "10:00am", 
+            "11:00am", 
+            "12:00pm", 
+            "1:00pm",
+            "2:00pm",
+            "3:00pm", 
+            "4:00pm", 
+            "5:00pm"
+        ], 
+        userEntry : ["","","","","","","","",""]
+        //, 
+        // hour : ["moment('9:00am', 'h:mma')" , 
+        // moment('10:00am', 'h:mma') ,
+        // moment('11:00am', 'h:mma') ,
+        // moment('12:00am', 'h:mma') ,
+        // moment('1:00pm', 'h:mma') ,
+        // moment('2:00pm', 'h:mma') ,
+        // moment('3:00pm', 'h:mma') ,
+        // moment('4:00pm', 'h:mma') ,
+        // moment('5:00pm', 'h:mma') ,]
     };
 
 
@@ -37,10 +58,19 @@ $(document).ready(function() {
             $(divEl).append(inputEl);
             $(divEl).append(buttonEl);
 
+            if (moment(schedule.time[i],'h:mma').isBefore(now)){
+                inputEl.addClass("past");
+            } else if(moment(schedule.time[i],'h:mma').isSame(now)){
+                inputEl.addClass("present");
+            }else if (moment(schedule.time[i],'h:mma').isAfter(now)){
+                inputEl.addClass("future");
+            }
+
             
 
         }
 
+        console.log(schedule)
 
     };
 
