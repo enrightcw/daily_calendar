3f84b23d5358a412662ef33e7d78dd2d8be52079
enrightcw <58670012+enrightcw@users.noreply.github.com>
2021-01-07 18:59:16 -0500

    Updated the whole project

--- README.md
+++ README.md
@@ -1,5 +1,47 @@
+
 # homeworkFive
+#### Author: enrightcw
 
-Worked on this with Tyler, despite trying to go big or go home, we could not get the local storage to save appropriately and render to the page.
+Created a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
-Colors work though.
+This app uses [Moment.js](https://momentjs.com/) to work with dates and times.
+
+### Open Issues <img src= "https://img.shields.io/github/issues/enrightcw/homeworkFive">
+
+# Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Tests](#tests)
+* [Questions](#questions)
+* [Contributors](#contributors)
+* [License](#license)
+
+## Installation
+
+none
+
+## Usage
+
+n/a
+
+## Tests
+
+none
+
+## Questions
+
+Using the email link below, please reach out with any questions.
+
+### Picture
+<img src="https://avatars2.githubusercontent.com/u/58670012?v=4">
+
+### Email 
+enrightcw@gmail.com
+
+## Contributors
+
+
+
+## License
+
+<img src="https://img.shields.io/github/license/enrightcw/homeworkFive">
--- script.js
+++ script.js
@@ -1,105 +1,55 @@
-//moment
-var NowMoment = moment().format("dddd, MMMM Do");
-var eDisplayMoment = document.getElementById('currentDay');
-eDisplayMoment.innerHTML = NowMoment;
-
-
 $(document).ready(function() {
-
-    //var declarations
-    var now = moment().hours();
-    
-    var schedule = { 
-        time : [
-            {display: "9:00am",
-        value: 9},
-            {display: "10:00am",
-        value: 10}, 
-            {display: "11:00am",
-        value: 11}, 
-            {display: "12:00pm",
-        value: 12}, 
-            {display: "1:00pm",
-        value: 13},
-            {display: "2:00pm",
-        value: 14},
-            {display: "3:00pm",
-        value: 15}, 
-            {display: "4:00pm", 
-        value: 16}, 
-            {display: "5:00pm",
-        value: 17}
-        ]        
-    };
-
-    //it checks the time within the hour element, to see where it falls within moment, and color .description accordingly.
-    function render(){
-        for( let i = 0 ; i < schedule.time.length ; i++) {
-            var divEl = $("<div>");
-            divEl.addClass("row hour");
-            var timeEl = $("<h4>");
-            var inputEl = $("<input>");
-            inputEl.addClass("description").attr("textarea-"+ i);
-            var buttonEl = $("<button>");
-            buttonEl.attr("data-textarea", i)
-            buttonEl.addClass("btn btn-default far fa-save saveBtn");
-            
-            $(timeEl).text(schedule.time[i].display);
-           
-            $(".time-block").append(divEl);
-            $(divEl).append(timeEl).append(inputEl);
-            $(divEl).append(buttonEl);
-            
-            //change colors based on time
-            if (schedule.time[i].value < now){
-                inputEl.addClass("past");
-            }else if (schedule.time[i].value === now){
-                inputEl.removeClass("past");
-                inputEl.addClass("present");
-                inputEl.addClass("present");
-
-            }else{ (schedule.time[i].value > now)
-                inputEl.removeClass("past");
-                inputEl.removeClass("present");
-                inputEl.addClass("future");
-            };   
-
-            
-            $("button").click(function(e){
-                e.preventDefault();
-
-                console.log("__________________")
-                console.log("clicked");
-    
-                var textarea = "textarea-" + $(this).attr("data-textarea");
-                var value = $("#" + textarea).val().trim();
-                
-                localStorage.setItem(textarea, value);
-                
-            });
-    
-            function renderSavedItems(){
-                for (let i = 0; i < 9 ; i++){
-                    var textarea = "textarea-" + i;
-                    console.log(textarea)
-
-                    var value = localStorage.getItem(textarea);
-                    console.log(value)
-                    $("#" + textarea).val(value);
-                }
-            }
+    // listen for save button clicks
+    $(".saveBtn").on("click", function() {
+      // get nearby values
+      var value = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+  
+      // save in localStorage
+      localStorage.setItem(time, value);
+    });
+  
+    function hourUpdater() {
+      // get current number of hours
+      var currentHour = moment().hours();
+  
+      // loop over time blocks
+      $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+  
+        // check if we've moved past this time
+        if (blockHour < currentHour) {
+          $(this).addClass("past");
+        } 
+        else if (blockHour === currentHour) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+        } 
+        else {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
         }
-
-        console.log(schedule)
-        console.log(now);
-        console.log(moment()._d);
-
-        //function for local storage of descInput, saves to local storage as a string
-        
-        
-        renderSavedItems();
-        
-    };
-
-    render()
-});
+      });
+    }
+  
+    hourUpdater();
+  
+    // set up interval to check if current time needs to be updated
+    var interval = setInterval(hourUpdater, 15000);
+  
+    // load any saved data from localStorage
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  
+    // display current day on page
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+  });
+  
